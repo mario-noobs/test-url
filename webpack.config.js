b801912eda321eeafd0e903d4e282b4102d82d9c
Dung Bui <dung.bui@littlebigconnection.com>
2025-11-26 16:47:05 +0700

    Add files

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   output: {
-    path: join(__dirname, './dist'),
+    path: join(__dirname, './dist/apps/test-url'),
   },
   plugins: [
     new NxAppWebpackPlugin({
